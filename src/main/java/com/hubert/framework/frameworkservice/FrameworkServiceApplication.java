3440a94df138c80b9c057d128f0ba547556f9e17
zhengxiaobin <xiaobin.zheng@hand-china.com>
2017-07-17 16:10:51 +0800

    first commit

---
+++ src/main/java/com/hubert/framework/frameworkservice/FrameworkServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hubert.framework.frameworkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hubert/framework/frameworkservice/FrameworkServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hubert.framework.frameworkservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrameworkServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
